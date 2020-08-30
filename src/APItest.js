5f990af9c76a37d1d10395ca631f4f2d3bb9a6ed
wags83 <gpwagner@gmail.com>
2020-08-30 17:49:45 -0400

    working on getting API data

---
+++ src/APItest.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class APItest extends React.Component {
+    
+    state = {
+        data: {}
+    }
+
+    componentDidMount = () => {
+        const configObj = {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'x-api-token': 'AIzaSyAg9vTI59ac2X9pl3I41TH8lmdMf2h51fM'
+              }, 
+            }
+            fetch(`https://gs.cscapi.com/data/getNFTMasterData`, configObj)
+            .then(resp => resp.json())
+            .then(result => console.log(result))
+        }
+
+        render () {
+            return null
+        }        
+    }
+export default APItest;
